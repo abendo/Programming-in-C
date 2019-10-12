e10ad5965d08cfc1ffe2f8766048f4fff672ec69
Albrit Bendo <48356107+abendo@users.noreply.github.com>
2019-10-12 14:54:56 +0200

    Update main.c

--- Programming in C 1/Assignment1.2/main.c
+++ Programming in C 1/Assignment1.2/main.c
@@ -11,3 +11,17 @@ printf("The result is %d\n", result);
 return 0;
 }
 
+/*
+Fix the program below such that it prints the correct value. Why does the program print “The
+result is -1073745604”? (Values will vary). Write your answer and explanations within
+comments.
+
+#include <stdio.h>
+int main() {
+int result; / ∗ The result of our calculation
+result = (2 + 7) ∗ 9 / 3;
+printf("The result is %d\n");
+return 0;
+}
+
+*/
